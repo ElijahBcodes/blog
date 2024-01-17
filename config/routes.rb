dae028da6f64836baeefe537ac5ce47c0293c835
ElijahBcodes <51147388+ElijahBcodes@users.noreply.github.com>
2024-01-16 16:34:39 -0800

    edit funtion operational

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
   post 'blog_posts' => 'blog_post#create' 
   delete 'blog_posts/:id' => 'blog_post#destroy', as: 'delete_blog'
   patch 'blog_posts/:id' => 'blog_post#update'
-  get 'blog_post/:id/edit' => 'blog_post#edit', as: 'edit_blog_post'
+
+  get 'blog_posts/:id/edit' => 'blog_post#edit', as: 'edit_blog_post'
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
